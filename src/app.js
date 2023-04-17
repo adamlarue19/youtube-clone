b162a73d68d2d5881118bd48d282ce0098b77c15
adamlarue19 <111079858+adamlarue19@users.noreply.github.com>
2023-04-17 15:13:05 -0400

    added components and utils, completed navbar and search bar

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material'
 
-const app = () => {
-  return (
-    <div>app</div>
-  )
-}
+
+import { Navbar, Feed, ChannelDetails, SearchFeed, VideoDetails } from './components';
+const app = () => (
+    <BrowserRouter>
+        <Box sx={{ backgroundColor: '#000' }}>
+            <Navbar />
+            <Routes>
+                <Route path='/' exact element={<Feed />} />
+                <Route path='/video/:id' exact element={<VideoDetails />} />
+                <Route path='/channel/:id' exact element={<ChannelDetails />} />
+                <Route path='/video/:id' exact element={<SearchFeed />} />
+            </Routes>
+        </Box>
+    </BrowserRouter>
+)
 
 export default app
