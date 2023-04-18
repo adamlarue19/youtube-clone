e4fef2a80adbd70c44fd34639a2b8137044b67f6
adamlarue19 <111079858+adamlarue19@users.noreply.github.com>
2023-04-18 18:01:44 -0400

    added video cards, channel card, channel details

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,26 +1,44 @@
-import {useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { Box, Stack, Typography } from '@mui/material'
-import Sidebar from './Sidebar'
 
+
+import { fetchFromAPI } from '../utils/fetchFromAPI'
+import { Sidebar, Videos } from './'
 
 const Feed = () => {
-  return (
-   <Stack sx={{flexDirection: {sx: 'column', md: 'row'} }}>
-      <Box sx= {{ height: {sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: {sx:0, md: 2 }}}>
-        <Sidebar />
 
-        <Typography className='copyright' variant='body2' sx={{mt: 1.5, color: '#fff'}}>
+  const [selectedCategory, setSelectedCategory] = useState('New');
+const [videos, setVideos] = useState([])
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    .then((data) => setVideos(data.items))
+  },[selectedCategory]);
+
+  return (
+    <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
+      <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
+        <Sidebar
+          selectedCategory=
+          {selectedCategory}
+          setSelectedCategory=
+          {setSelectedCategory}
+        />
+
+        <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff' }}>
           Copyright 2023 Adam Larue
         </Typography>
       </Box>
 
-      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
         <Typography variant='h4'
-        fontWeight="bold" md={2} sx={{ color: 'white'}}>
-         New <span style={{color: '#f31503'}}> videos</span>
+          fontWeight="bold" md={2} sx={{ color: 'white' }}>
+          {selectedCategory} <span style={{ color: '#f31503' }}> videos</span>
         </Typography>
+
+        <Videos videos={videos} />
+
       </Box>
-   </Stack>
+    </Stack>
   )
 }
 
